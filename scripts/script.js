bb5374a52824cc8f62fbab16b3efb628ed19e99d
SuperJoy <superjoy0502@gmail.com>
2021-06-16 10:03:52 -0600

    Code Cleaning + Check Questions After

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,61 @@
+//region Basic jQuery Settings
 $(document).ready(function () {
-	$(".stuff").fadeIn("slow");
+    $(".stuff").fadeIn("slow");
 });
 
 $("#interestslink").click(function () {
-	$.when($(".stuff").fadeOut("slow")).done(function () {
-		window.location.href = "/pages/interests.html";
-	});
+    $.when($(".stuff").fadeOut("slow")).done(function () {
+        window.location.href = "/pages/interests.html";
+    });
 });
 
 $("#organizationslink").click(function () {
-	$.when($(".stuff").fadeOut("slow")).done(function () {
-		window.location.href = "/pages/organizations.html";
-	});
+    $.when($(".stuff").fadeOut("slow")).done(function () {
+        window.location.href = "/pages/organizations.html";
+    });
 });
 
 $("#homelink").click(function () {
-	$.when($("body").fadeOut("slow")).done(function () {
-		window.location.href = "/index.html";
-	});
+    $.when($("body").fadeOut("slow")).done(function () {
+        window.location.href = "/index.html";
+    });
 });
 
 $("#contactlink").click(function () {
-	$.when($(".stuff").fadeOut("slow")).done(function () {
-		window.location.href = "/pages/contact.html";
-	});
+    $.when($(".stuff").fadeOut("slow")).done(function () {
+        window.location.href = "/pages/contact.html";
+    });
 });
 
 $("#aboutlink").click(function () {
-	$.when($(".stuff").fadeOut("slow")).done(function () {
-		window.location.href = "/pages/about.html";
-	});
+    $.when($(".stuff").fadeOut("slow")).done(function () {
+        window.location.href = "/pages/about.html";
+    });
 });
 
 $("#startButton").click(function () {
-	$.when($(".stuff").fadeOut("slow")).done(function () {
-		fetch("/resources/quiz.json", {
-			headers: {
-				"Content-Type": "application/json",
-				Accept: "application/json",
-			},
-		})
-			.then((response) => response.text())
-			.then((text) => loadQuiz(text));
-	});
+    $.when($(".stuff").fadeOut("slow")).done(function () {
+        fetch("/resources/quiz.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .then((response) => response.text())
+            .then((text) => loadQuiz(text));
+    });
 });
+//endregion
 
+//region Variables
 let slider = document.getElementById("questions");
-let questionNum;
-let quizObj;
-let quizGiven = [];
-let multipleChoice = document.getElementById("MC");
 let shortAnswer = document.getElementById("SA");
+let multipleChoice = document.getElementById("MC");
 let questionText = document.getElementById("question");
+let textField = document.getElementById("textAnswer");
 let buttons = document.getElementById("buttons");
+let nextButton = document.getElementById("next");
+let previousButton = document.getElementById("previous");
 let a = document.getElementById("A");
 let b = document.getElementById("B");
 let c = document.getElementById("C");
@@ -61,6 +64,9 @@ let aText = document.getElementById("AText");
 let bText = document.getElementById("BText");
 let cText = document.getElementById("CText");
 let dText = document.getElementById("DText");
+let questionNum;
+let quizObj;
+let quizGiven = [];
 let correctAnswers = [];
 let choices = [];
 let choicesData = [];
@@ -69,214 +75,223 @@ let pos = -1;
 let answers = [];
 let questionsCorrect = [];
 let quizEnd = false;
+//endregion
+
+//region Quiz Script
+//region Early Settings
+if (slider) {
+    slider.addEventListener("input", updateText);
+    updateText();
+}
+
+if (nextButton && previousButton) {
+    nextButton.addEventListener("click", nextQuestion);
+    previousButton
+        .addEventListener("click", previousQuestion);
+}
 
 function updateText() {
-	questionNum = slider.value;
-	document.getElementById("number").innerHTML = questionNum;
-}
-
-if (slider) {
-	slider.addEventListener("input", updateText);
-	updateText();
-}
-
-if (document.getElementById("next") && document.getElementById("previous")) {
-	document.getElementById("next").addEventListener("click", nextQuestion);
-	document
-		.getElementById("previous")
-		.addEventListener("click", previousQuestion);
+    questionNum = slider.value;
+    document.getElementById("number").innerHTML = questionNum;
 }
 
 function loadQuiz(json) {
-	quizObj = String(json).replace(/\t/g, "");
-	quizObj = String(quizObj).replace(/\n/g, "");
-	quizObj = JSON.parse(quizObj);
-	let numbers = [];
-	for (let i = 0; i < questionNum; i++) {
-		let randNum = Math.floor(Math.random() * quizObj.quiz.length);
-		if (numbers.includes(randNum)) {
-			i--;
-			continue;
-		}
-		numbers.push(randNum);
-		quizGiven.push(quizObj.quiz[randNum]);
-		switch (quizObj.quiz[randNum].type) {
-			case "MC":
-				let numbers = [];
-				choices = [];
-				for (let j = 0; j < 4; j++) {
-					let randNum = Math.floor(Math.random() * 4);
-					if (numbers.includes(randNum)) {
-						j--;
-						continue;
-					}
-					numbers.push(randNum);
-					switch (j) {
-						case 0:
-							choices.push(quizGiven[i].choices[randNum]);
-							break;
-						case 1:
-							choices.push(quizGiven[i].choices[randNum]);
-							break;
-						case 2:
-							choices.push(quizGiven[i].choices[randNum]);
-							break;
-						case 3:
-							choices.push(quizGiven[i].choices[randNum]);
-							break;
-						default:
-							alert(
-								"OH NO WHY DID THIS HAPPEN??? PLEASE RELOAD :("
-							);
-					}
-				}
-				correctAnswers.push(quizObj.quiz[randNum].answer);
-				choicesData.push(choices);
-				break;
-			case "SA":
-				correctAnswers.push(quizObj.quiz[randNum].answers);
-				choicesData.push(null);
-				break;
-			default:
-				alert(
-					"There is an unvalid data in the JSON file. Please reload."
-				);
-		}
-	}
-	console.log(quizGiven);
-	nextQuestion();
-	$("#quizcontainer").fadeIn("slow");
+    quizObj = String(json).replace(/\t/g, "");
+    quizObj = String(quizObj).replace(/\n/g, "");
+    quizObj = JSON.parse(quizObj);
+    let numbers = [];
+    for (let i = 0; i < questionNum; i++) {
+        let randNum = Math.floor(Math.random() * quizObj.quiz.length);
+        if (numbers.includes(randNum)) {
+            i--;
+            continue;
+        }
+        numbers.push(randNum);
+        quizGiven.push(quizObj.quiz[randNum]);
+        switch (quizObj.quiz[randNum].type) {
+            case "MC":
+                let numbers = [];
+                choices = [];
+                for (let j = 0; j < 4; j++) {
+                    let randNum = Math.floor(Math.random() * 4);
+                    if (numbers.includes(randNum)) {
+                        j--;
+                        continue;
+                    }
+                    numbers.push(randNum);
+                    switch (j) {
+                        case 0:
+                            choices.push(quizGiven[i].choices[randNum]);
+                            break;
+                        case 1:
+                            choices.push(quizGiven[i].choices[randNum]);
+                            break;
+                        case 2:
+                            choices.push(quizGiven[i].choices[randNum]);
+                            break;
+                        case 3:
+                            choices.push(quizGiven[i].choices[randNum]);
+                            break;
+                        default:
+                            alert(
+                                "OH NO WHY DID THIS HAPPEN??? PLEASE RELOAD :("
+                            );
+                    }
+                }
+                correctAnswers.push(quizObj.quiz[randNum].answer);
+                choicesData.push(choices);
+                break;
+            case "SA":
+                correctAnswers.push(quizObj.quiz[randNum].answers);
+                choicesData.push(null);
+                break;
+            default:
+                alert(
+                    "There is an unvalid data in the JSON file. Please reload."
+                );
+        }
+    }
+    console.log(quizGiven);
+    nextQuestion();
+    $("#quizcontainer").fadeIn("slow");
 }
+//endregion
 
+//region UI/UX
 function updateUI() {
-	$("input").prop("checked", false);
-	$("input").val("");
-	currentQuestion = quizGiven[pos];
-	if (currentQuestion.type == "MC") {
-		multipleChoice.style.display = "inline";
-		shortAnswer.style.display = "none";
-		a.value = choicesData[pos][0];
-		b.value = choicesData[pos][1];
-		c.value = choicesData[pos][2];
-		d.value = choicesData[pos][3];
-		aText.innerHTML = choicesData[pos][0];
-		bText.innerHTML = choicesData[pos][1];
-		cText.innerHTML = choicesData[pos][2];
-		dText.innerHTML = choicesData[pos][3];
-		for (let i = 0; i < 4; i++) {
-			switch (i) {
-				case 0:
-					if (a.value == answers[pos]) {
-						$("#A").prop("checked", true);
-					}
-					break;
-				case 1:
-					if (b.value == answers[pos]) {
-						$("#B").prop("checked", true);
-					}
-					break;
-				case 2:
-					if (c.value == answers[pos]) {
-						$("#C").prop("checked", true);
-					}
-					break;
-				case 3:
-					if (d.value == answers[pos]) {
-						$("#D").prop("checked", true);
-					}
-					break;
-				default:
-					alert("This isn't supposed to happen. Please reload.");
-			}
-		}
-	} else if (currentQuestion.type == "SA") {
-		shortAnswer.style.display = "inline";
-		multipleChoice.style.display = "none";
-		if (answers[pos]) {
-			document.getElementById("textAnswer").value = answers[pos];
-		}
-	}
-	questionText.innerHTML = quizGiven[pos].question;
+    $("input").prop("checked", false);
+    $("input").val("");
+    currentQuestion = quizGiven[pos];
+    if (currentQuestion.type == "MC") {
+        multipleChoice.style.display = "inline";
+        shortAnswer.style.display = "none";
+        a.value = choicesData[pos][0];
+        b.value = choicesData[pos][1];
+        c.value = choicesData[pos][2];
+        d.value = choicesData[pos][3];
+        aText.innerHTML = choicesData[pos][0];
+        bText.innerHTML = choicesData[pos][1];
+        cText.innerHTML = choicesData[pos][2];
+        dText.innerHTML = choicesData[pos][3];
+        for (let i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    if (a.value == answers[pos]) {
+                        $("#A").prop("checked", true);
+                    }
+                    break;
+                case 1:
+                    if (b.value == answers[pos]) {
+                        $("#B").prop("checked", true);
+                    }
+                    break;
+                case 2:
+                    if (c.value == answers[pos]) {
+                        $("#C").prop("checked", true);
+                    }
+                    break;
+                case 3:
+                    if (d.value == answers[pos]) {
+                        $("#D").prop("checked", true);
+                    }
+                    break;
+                default:
+                    alert("This isn't supposed to happen. Please reload.");
+            }
+        }
+    } else if (currentQuestion.type == "SA") {
+        shortAnswer.style.display = "inline";
+        multipleChoice.style.display = "none";
+        if (answers[pos]) {
+            textField.value = answers[pos];
+        }
+    }
+    questionText.innerHTML = quizGiven[pos].question;
 }
 
 function previousQuestion() {
-	if (pos < 1) return;
-	if (currentQuestion.type == "MC") {
-		answers[pos] = $(
-			"input[name='MultipleChoice']:checked",
-			"#mcForm"
-		).val();
-	} else if (currentQuestion.type == "SA") {
-		answers[pos] = document.getElementById("textAnswer").value;
-	}
-	pos--;
-	updateUI();
+    if (pos < 1) return;
+    if (currentQuestion.type == "MC") {
+        answers[pos] = $(
+            "input[name='MultipleChoice']:checked",
+            "#mcForm"
+        ).val();
+    } else if (currentQuestion.type == "SA") {
+        answers[pos] = textField.value;
+    }
+    pos--;
+    updateUI();
 }
 
 function nextQuestion() {
-	if (pos !== -1) {
-		if (currentQuestion.type == "MC") {
-			answers[pos] = $(
-				"input[name='MultipleChoice']:checked",
-				"#mcForm"
-			).val();
-		} else if (currentQuestion.type == "SA") {
-			answers[pos] = document.getElementById("textAnswer").value;
-		}
-	}
-	pos++;
-	choices = [];
-	currentQuestion = quizGiven[pos];
-	if (pos == quizGiven.length) {
-		multipleChoice.style.display = "none";
-		shortAnswer.style.display = "none";
-		buttons.style.display = "none";
-		checkQuiz();
-		return;
-	}
-	questionText.innerHTML = currentQuestion.question;
-	updateUI();
+    if (pos !== -1) {
+        if (currentQuestion.type == "MC") {
+            answers[pos] = $(
+                "input[name='MultipleChoice']:checked",
+                "#mcForm"
+            ).val();
+        } else if (currentQuestion.type == "SA") {
+            answers[pos] = textField.value;
+        }
+    }
+    pos++;
+    choices = [];
+    currentQuestion = quizGiven[pos];
+    if (pos == quizGiven.length) {
+        checkQuiz();
+        return;
+    }
+    questionText.innerHTML = currentQuestion.question;
+    updateUI();
 }
+//endregion
 
+//region Later Processing
 function checkQuiz() {
-	for (let i = 0; i < correctAnswers.length; i++) {
-		if (typeof correctAnswers[i] == "string") {
-			if (answers[i] == correctAnswers[i]) {
-				questionsCorrect.push(true);
-			} else {
-				questionsCorrect.push(false);
-			}
-		} else if (typeof correctAnswers[i] == "object") {
-			if (correctAnswers[i].includes(answers[i])) {
-				questionsCorrect.push(true);
-			} else {
-				questionsCorrect.push(false);
-			}
-		} else {
-			alert("Unexpected Error. Please Reload.");
-		}
-	}
-	var count = 0;
-	for (var i = 0; i < questionsCorrect.length; ++i) {
-		if (questionsCorrect[i] == true) count++;
-	}
-	let result = Math.round((count / questionsCorrect.length) * 100);
-	questionText.innerHTML = `Quiz Finished: ${result}%`;
-	document.getElementById("mcForm").readOnly = true;
-	document.getElementById("textAnswer").readOnly = true;
-	quizEnd = true;
+    for (let i = 0; i < correctAnswers.length; i++) {
+        if (typeof correctAnswers[i] == "string") {
+            if (answers[i] == correctAnswers[i]) {
+                questionsCorrect.push(true);
+            } else {
+                questionsCorrect.push(false);
+            }
+        } else if (typeof correctAnswers[i] == "object") {
+            if (correctAnswers[i].includes(answers[i])) {
+                questionsCorrect.push(true);
+            } else {
+                questionsCorrect.push(false);
+            }
+        } else {
+            alert("Unexpected Error. Please Reload.");
+        }
+    }
+    let count = 0;
+    for (let i = 0; i < questionsCorrect.length; ++i) {
+        if (questionsCorrect[i] == true) count++;
+    }
+    let result = Math.round((count / questionsCorrect.length) * 100);
+    questionText.innerHTML = `Quiz Finished: ${result}%`;
+    multipleChoice.readOnly = true;
+    textField.readOnly = true;
+    quizEnd = true;
+    pos = 0;
+    updateUI();
 }
+//endregion
+//endregion
 
+//region Collapsible Elements
 let coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function () {
-		this.classList.toggle("active");
-		let content = this.nextElementSibling;
-		if (content.style.maxHeight) {
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
+//endregion
